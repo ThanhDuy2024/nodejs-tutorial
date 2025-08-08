bedc38beca8d87de0103ebac98a2895632944730
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-08-08 09:33:21 +0700

    [Feature] account admin register

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,5 +1,29 @@
+import bcrypt from "bcryptjs";
 import AccountAdmin from "../../models/accountAdmin.model.js";
+
 export const register = async (req, res) => {
+
+  const { fullName, email, password } = req.body;
+  
+  const checkAccount = await AccountAdmin.findOne(
+    {
+      email: email,
+    }
+  )
+
+  if(checkAccount) {
+    res.json({
+      code: "error",
+      message: "Your email is exists!"
+    });
+    return;
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  
+  req.body.password = hash;
+
   const newAccountAdmin = new AccountAdmin(req.body);
   await newAccountAdmin.save(); //0.5s
   res.json(
@@ -9,10 +33,3 @@ export const register = async (req, res) => {
     }
   )
 }
-
-//fullName
-//email
-//password
-//image
-//adress
-//roleid
